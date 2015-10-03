e51ff04c42913fd5b9b4fefaeaeb2cad1690ab02
Ksenia Shikhova <kseniashikhova@Ksenias-MacBook-Pro-2.local>
2015-10-02 20:25:06 -0400

    The switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
+$('.switch button').on('click',function(e){
+		$(this).toggleClass('on');
+	
+});
+	
+$('.switch button').click(function() {
+		if ($(this).hasClass('on')) {
+		$('body').css('background-color','black');
+		$('.status').css('color','white');
+		$('.status').text("Hey, who turn off the lights?");}
+});
 
 
+		$('.switch button').click(function() {
+		if ($(this).hasClass('off')) {
+		$('body').css('background-color','white');
+		$('.status').css('color','black');
+		$('.status').text("It's so bright in here!");}
+});
