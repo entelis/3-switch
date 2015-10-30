f0a061e5ceb426f95ca61332606f7567b1818573
Ksenia Shikhova <kseniashikhova@Ksenias-MacBook-Pro-2.local>
2015-10-30 17:16:05 -0400

    edited file

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
-$('.switch button').on('click',function(e){
-		$(this).toggleClass('on');
-	
-});
-	
-$('.switch button').click(function() {
-		if ($(this).hasClass('on')) {
-		$('body').css('background-color','black');
-		$('.status').css('color','white');
-		$('.status').text("Hey, who turn off the lights?");}
-});
-
-
-		$('.switch button').click(function() {
-		if ($(this).hasClass('off')) {
-		$('body').css('background-color','white');
-		$('.status').css('color','black');
-		$('.status').text("It's so bright in here!");}
+$('.on').on('click', function(e) {
+	if ($(this).hasClass('on')) {
+		$(this).removeClass('on').addClass('off');
+		$('.light').removeClass('light').addClass('dark');
+		$('.status').css("color", "white").text("Who turned off the lights?");
+	}
+	else {
+		$(this).removeClass('off').addClass('on');
+		$('.dark').removeClass('dark').addClass('light');
+		$('.status').css("color", "black").text("It's so bright in here!");
+	};
 });
